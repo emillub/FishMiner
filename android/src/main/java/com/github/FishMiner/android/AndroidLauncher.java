184252861c59cc63eae4d05b3c4fb7dddeccf2b0
Jespese <jespese@stud.ntnu.no>
2025-03-06 17:29:11 +0100

    feat: commit unversioned files

---
+++ android/src/main/java/com/github/FishMiner/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.github.FishMiner.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.github.FishMiner.FishMinerGame;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new FishMinerGame(), configuration);
+    }
+}
