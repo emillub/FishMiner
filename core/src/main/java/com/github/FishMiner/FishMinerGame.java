8c1eb3117e3fb62f459725b8e4e7c43849e95b4f
Jespese <jespese@stud.ntnu.no>
2025-03-06 14:01:16 +0100

    Remove MVC template

--- core/src/main/java/com/github/FishMiner/FishMinerGame.java
+++
@@ -1,8 +0,0 @@
-package com.github.FishMiner;
-
-/** This class serves only as the application scanning root. Any classes in its package (or any of the sub-packages)
- * with proper Autumn MVC annotations will be found, scanned and initiated. */
-public class FishMinerGame {
-    /** Default application size. */
-    public static final int WIDTH = 450, HEIGHT = 600;
-}
--- core/src/main/java/com/github/FishMiner/service/controls/ControlListener.java
+++
@@ -1,7 +0,0 @@
-package com.github.FishMiner.service.controls;
-
-/** Listens to game events. */
-public interface ControlListener {
-    /** Invoked when controller detects jump input event. */
-    void jump();
-}
